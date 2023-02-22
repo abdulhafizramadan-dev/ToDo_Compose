f179df76a0712eccc916e3cccf956a4534542fc9
abdulhafizramadan-dev <abdulhafizramadan.dev@gmail.com>
2023-02-22 23:10:19 +0700

    | Setup Hilt

---
+++ app/src/main/java/com/ahr/todo_compose/di/DatabaseModule.kt
@@ -0,0 +1,36 @@
+package com.ahr.todo_compose.di
+
+import android.content.Context
+import androidx.room.Room
+import com.ahr.todo_compose.data.TodoDao
+import com.ahr.todo_compose.data.TodoDatabase
+import com.ahr.todo_compose.util.Constants.DATABASE_NAME
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ): TodoDatabase = Room.databaseBuilder(
+        context,
+        TodoDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Binds
+    fun provideTodoDao(
+        todoDatabase: TodoDatabase
+    ): TodoDao = todoDatabase.todoDao()
+
+}
