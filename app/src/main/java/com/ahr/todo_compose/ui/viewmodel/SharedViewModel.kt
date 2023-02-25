2fbcaa05a02959fc40454e546e20668b5104c5d9
abdulhafizramadan-dev <abdulhafizramadan.dev@gmail.com>
2023-02-25 14:48:37 +0700

    | TaskScreen - Delete Task

--- app/src/main/java/com/ahr/todo_compose/ui/viewmodel/SharedViewModel.kt
+++ app/src/main/java/com/ahr/todo_compose/ui/viewmodel/SharedViewModel.kt
@@ -76,7 +76,7 @@ class SharedViewModel @Inject constructor(
         when (action) {
             Action.ADD -> addTask()
             Action.UPDATE-> updateTask()
-            Action.DELETE->{}
+            Action.DELETE-> deleteTask()
             Action.DELETE_ALL->{}
             Action.UNDO->{}
             else ->{}
@@ -104,6 +104,18 @@ class SharedViewModel @Inject constructor(
                 priority = priority.value
             )
             repository.updateTask(todoTask = todoTask)
+        }
+    }
+
+    private fun deleteTask() {
+        viewModelScope.launch {
+            val todoTask = TodoTask(
+                id = id.value,
+                title = title.value,
+                description = description.value,
+                priority = priority.value
+            )
+            repository.deleteTask(todoTask = todoTask)
         }
     }
 }
